fdc16ecec842d2288a8f8653a461ba40bec6e36e
Frank Hellwig <frank@hellwig.org>
2022-05-28 14:11:36 -0400

    host

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ import express from 'express';
 export function createApp(distDir) {
   const app = express();
   app.enable('trust proxy');
+  app.get('/hostname', (req, res) => {
+    res.send(req.headers.host);
+  });
   app.use(express.static(distDir));
   return app;
 }
