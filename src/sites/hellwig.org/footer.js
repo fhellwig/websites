25f39f125223c01e8653c3e5940710d711788936
Frank Hellwig <frank@hellwig.org>
2022-07-23 10:57:27 -0400

    footer

--- src/sites/hellwig.org/footer.js
+++ src/sites/hellwig.org/footer.js
@@ -1,24 +1,33 @@
 import styled from 'styled-components';
-import { Content, Footer as BulmaFooter } from './bulma.js';
+import { Content } from './bulma.js';
 import trust from './positivessl.png';
 
-const StyledFooter = styled(BulmaFooter)`
+const StyledFooter = styled.footer`
+  height: 70px;
+  line-height: 70px;
+  text-align: center;
   color: white;
   background-color: cornflowerblue;
+`;
+
+const FooterPara = styled.p`
+  margin-right: 1em;
+`;
+
+const FooterImg = styled.img`
+  float: right;
+  margin-top: 20px;
 `;
 
 export function Footer() {
   return (
     <StyledFooter>
       <Content>
-        <p>
+        <FooterPara>
           Please contact me via <a href="tel:5712076308">phone</a> or{' '}
-          <a href="mailto:frank@hellwig.org">email</a>
-        </p>
-        <p>
-          <img src={trust} />
-        </p>
-        <p>Build: 220723.02</p>
+          <a href="mailto:frank@hellwig.org">email</a> (Build: 220723.02)
+          <FooterImg src={trust} />
+        </FooterPara>
       </Content>
     </StyledFooter>
   );
