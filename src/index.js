2c9b03699aa40cbc78a131b6a407cc96f6233c35
Frank Hellwig <frank@hellwig.org>
2022-07-23 19:09:47 -0400

    restructure

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import 'bulma/css/bulma.css';
 import { createRoot } from 'react-dom/client';
-import { getApp } from './app-selector.js';
+import { App } from './app.js';
 import './styles.css';
 
-const container = document.getElementById('app');
+const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(getApp());
+root.render(<App />);
--- src/sites/odalco.com/app.js
+++
@@ -1,3 +0,0 @@
-export function App() {
-  return <h1>Want to lease a plane?</h1>;
-}
